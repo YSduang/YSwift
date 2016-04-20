debc083289bb0bdb46b1727216c94fdd78562802
Brent <brent7@163.com>
2016-04-20 11:33:56 +0800

    首次创建，测试git

---
+++ anySignPlugin/anySignPlugin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  anySignPlugin
+//
+//  Created by BJCA on 16/4/20.
+//  Copyright © 2016年 YSdang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
